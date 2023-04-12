816861c02923aad1b06be11352839a5ddb9c8604
decyphertek-io <ciphertek@protonmail.com>
2023-04-12 14:54:24 -0500

    decyphertek-io

--- bash-scripts/nmap.sh
+++ bash-scripts/nmap.sh
@@ -1,3 +1,3 @@
-sudo nmap -sn -Pn 192.168.0.0/24 | awk '/Nmap scan/{gsub(/[()]/,"",$NF); print $NF > "livehosts.txt"}'
+sudo nmap -sn 192.168.0.0/16 -oG - | awk '/Up$/{print $2}' > ips.txt
 sudo nmap -O --osscan-limit -iL livehosts.txt -oX livehosts-OS.xml 
 xsltproc livehosts-OS.xml -o livehosts-OS.html
