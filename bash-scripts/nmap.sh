e3c180f51e1f4003fe8b6a410447cd1c5726c04c
decyphertek-io <ciphertek@protonmail.com>
2023-04-21 13:50:59 -0500

    decyphertek-io

--- bash-scripts/nmap.sh
+++ bash-scripts/nmap.sh
@@ -1,3 +1,38 @@
-sudo nmap -sn 192.168.0.0/16 -oG - | awk '/Up$/{print $2}' > ips.txt
-sudo nmap -O --osscan-limit -iL livehosts.txt -oX livehosts-OS.xml 
-xsltproc livehosts-OS.xml -o livehosts-OS.html
+#!/bin/sh
+
+# IP variable
+echo -n " What is the IP or CIDR ?  "
+read -r IP
+
+# General Nmap Scan
+sudo nmap -sT -sV -sC -O -R $IP -oX general-scan.xml
+
+# Windows - SMB
+sudo nmap -sS -p 139,45 --script smb-os-discovery $IP -oX smb-discovery.xml
+
+# Switches - SNMP
+sudo nmap -sU -p 161 --script snmp-info $IP -oX snmp-info.xml
+
+# Firewall Bypass
+sudo nmap --script firewall-bypass $IP -oX firewall-bypass.xml
+
+# Vulnerability Scan
+sudo nmap --script vuln $IP -oX vuln-info.xml
+
+# merge 1 xml
+xmlstarlet sel -t -c "//node()"  smb-discovery.xml snmp-info.xml > nmap-merge-01.xml
+
+# merge 2 xml
+xmlstarlet -t -c "//node()"  firewall-bypass.xml vuln-info.xml > nmap-merge-02.xml
+
+# Merge the merge
+xmlstarlet -t -c "//node()" nmap-merge-01.xml nmap-merge-02.xml > nmap-merged.xml
+
+# Final merge
+xmlstarlet -t -c "//node()"  nmap-merged.xml general-scan.xml > nmap-merged-results.xml
+
+# Convert to html 
+xsltproc nmap-merged-results.xml -o nmap-merged-results.html
+
+# View the results in the terminal
+lynx nmap-merged-results.thml
